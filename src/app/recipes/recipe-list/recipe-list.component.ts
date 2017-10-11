b5e32a3a3327e30bd33e08baa18caeb38c7732fb
Juan Jo Ruiz Ferrer <jjgeek.sis@gmail.com>
2017-10-10 20:32:32 -0400

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes = [];
 
   constructor() { }
 
