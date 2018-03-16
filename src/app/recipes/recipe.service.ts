e27f1ac5488c8c778b101815d9dbcc073fb2fdd7
Juan Jo Ruiz Ferrer <jjgeek.sis@gmail.com>
2018-03-15 23:22:50 -0400

    changed forms strategy and overall changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  
+  recipesChanged = new Subject<Recipe[]>();
+  
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel', 
@@ -44,4 +48,18 @@ export class RecipeService {
     this.slService.addIngredients(ingredients);
   }
  
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  
+  updateRecipe(index:number, recipe:Recipe){
+    this.recipes[index] = recipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  
+  deleteRecipe(index:number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
